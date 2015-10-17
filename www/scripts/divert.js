3ec21933ce9713fd4809e738c785363a20d1a4ef
code-11 <brendan.ritter@yahoo.com>
2015-10-17 16:47:36 -0400

    Started on a possibly unused divert page between users and owners

---
+++ www/scripts/divert.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#owner-btn").click(function(){
+		window.location="login.html?1";
+	});
+
+	$("#user-btn").click(function(){
+		window.location="login.html?0";
+	});
+});
