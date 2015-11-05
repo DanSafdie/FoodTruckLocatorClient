404c1ec780a4d4cf3d5a4ebd46a77f9d2618532b
Daniel Safdie <dansafdie@dhcp-ccc-3617.eduroam.cornell.edu>
2015-11-05 12:18:59 -0500

    adjusted menu slide speed, fixed top bar sizing glitch, adjusted location of burger menu, made bottom banner appear at touch of truck.

--- www/scripts/main_menu.js
+++ www/scripts/main_menu.js
@@ -2,12 +2,12 @@ define(["jquery"],function($){
 	var main_menu = main_menu || {};
 	main_menu.show=function(){		
 		// $("#menu").show();
-		$("#menu").animate({left:"0px"},200);
+		$("#menu").animate({left:"0px"},150);
 		// $("#menu").animate({width:"toggle"},2000);
 		$("#menu-hider").show();
 	};
 	main_menu.hide=function(){
-		$("#menu").animate({left:"-75vw"},200);
+		$("#menu").animate({left:"-75vw"},150);
 		$("#menu-hider").hide();
 	}
 	return main_menu;
