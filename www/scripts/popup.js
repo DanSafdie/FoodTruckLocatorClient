82bb44240b458836d3b86cca47c782ba2febc757
Daniel Safdie <dansafdie@Daniels-MacBook-Pro.local>
2015-11-05 21:41:20 -0500

    fucked w the color scheme

--- www/scripts/popup.js
+++ www/scripts/popup.js
@@ -6,7 +6,7 @@ define(["jquery"],function($){
 	};
 	popup.hide=function(){
 		console.log("adsfasd");
-		$("#map-popup").animate({bottom:"-80"},150);
+		$("#map-popup").animate({bottom:"-85"},150);
 	}
 	return popup;
 });
