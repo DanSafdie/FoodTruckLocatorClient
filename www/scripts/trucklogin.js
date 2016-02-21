db076a1c624559c45143e10e64700d5d0f4375dd
code-11 <brendan.ritter@yahoo.com>
2016-02-21 15:40:38 -0500

    Added truck view

--- www/scripts/trucklogin.js
+++ www/scripts/trucklogin.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	$("#truckloginsubmit").click(function(){
 		var username=$("#username").val();
 		//TODO: DATABASE VALIDATION FOR USERNAME
-		window.location="mapview.html?"+username+"|1";
+		window.location="truckview.html?"+username+"|1";
 	});
 });
