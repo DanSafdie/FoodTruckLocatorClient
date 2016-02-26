b8c2e4eb71410c33774fdf381c8d33d38810a9fb
code-11 <brendan.ritter@yahoo.com>
2016-02-26 13:25:21 -0500

    Made a lot of style changes as well as pretotyped out the push to social media flow.

---
+++ www/scripts/socialpush.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var socials=document.getElementById("a1").addEventListener("click",function(){
+			document.getElementById("a1").style.backgroundColor="#6E6E6E";
+	});
+	var socials=document.getElementById("a2").addEventListener("click",function(){
+			document.getElementById("a2").style.backgroundColor="#6E6E6E";
+	});
+	var socials=document.getElementById("a3").addEventListener("click",function(){
+			document.getElementById("a3").style.backgroundColor="#6E6E6E";
+	});
+	var socials=document.getElementById("a4").addEventListener("click",function(){
+			document.getElementById("a4").style.backgroundColor="#6E6E6E";
+	});
+	var socials=document.getElementById("a4").addEventListener("click",function(){
+			document.getElementById("a4").style.backgroundColor="#6E6E6E";
+	});
+});
+
+
---
+++ www/scripts/truckview.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+		document.getElementById("deal").addEventListener("click",function(){
+		alert("Posted 1h 10% off deal!")
+	});
+});
+
+
