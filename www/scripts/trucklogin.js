e261fdab41d1333ef59e98cf16ee87029fbef307
code-11 <brendan.ritter@yahoo.com>
2016-05-04 18:29:44 -0400

    Changed the client js for truck login to work like client login

--- www/scripts/trucklogin.js
+++ www/scripts/trucklogin.js
@@ -7,4 +7,45 @@ $(document).ready(function(){
 	$("#trucklogin-signup").click(function(){
 		window.location="signup1.html";
 	});
+	OAuth.initialize('s6GkBwYgphGVqzw7xZG2ztdg3b8');
+
+	$("#facebook-login").click(function(){
+		OAuth.redirect('facebook', 'http://foodinatorclient.herokuapp.com/www/trucklogin.html');
+	});
+	$("#google-login").click(function(){
+		OAuth.redirect('google', 'http://foodinatorclient.herokuapp.com/www/trucklogin.html');
+	});
+	$("#twitter-login").click(function(){
+		OAuth.redirect('twitter', 'http://foodinatorclient.herokuapp.com/www/trucklogin.html');
+	});
+	OAuth.callback('google',function(error,success) {
+		if (typeof success !== "undefined") {
+			window.location="truckview.html?"+success.access_token+"|1";
+		}else{
+			console.log(error);
+		}
+	});
+	OAuth.callback('twitter',function(error,success) {
+		if (typeof success !== "undefined") {
+			// success.post('/1.1/statuses/update.json', {
+	  // 			data: {
+	  //   			status: "hello world!"
+	  // 			}
+			// });
+			window.location="truckview.html?"+success.oauth_token+"|1";
+		}else{
+			console.log(error);
+		}
+	});
+	OAuth.callback('facebook',function(error,success) {
+		if (typeof success !== "undefined") {
+			// console.log(success);
+			// success.post("/me/feed", {
+	  // 			message: "hello world!"
+			// });
+			window.location="truckview.html?"+success.access_token+"|1";
+		}else{
+			console.log(error);
+		}
+	});
 });
