ac38c189b66ba08ea1dd489d5e2bc9dbfe2d1dc5
code-11 <brendan.ritter@yahoo.com>
2016-05-05 16:37:06 -0400

    Seems to work, more testing

--- www/scripts/googlemap.js
+++ www/scripts/googlemap.js
@@ -95,8 +95,8 @@ requirejs(['async!https://maps.google.com/maps/api/js?key=AIzaSyBnOsVQzm27ZRMqj4
 
 	function validTruck(truck){
 		var a= typeof truck.tinfo !== "undefined";
-		console.log(a);
-		console.log(truck.tinfo);
+		// console.log(a);
+		// console.log(truck.tinfo);
 		if (a){
 			var b= typeof truck.lastpos !== "undefined";
 			var c= typeof truck.tinfo.tags !=="undefined";
