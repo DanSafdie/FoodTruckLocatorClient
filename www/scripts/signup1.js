f8fed569a1d7c4f7a7c805028857f2e8dee59cfe
code-11 <brendan.ritter@yahoo.com>
2016-05-04 23:07:06 -0400

    Once more with effort

--- www/scripts/signup1.js
+++ www/scripts/signup1.js
@@ -75,7 +75,7 @@ define(["jquery","nomadic_storage","jquery-ui","reporting"],function($,NS,JQUI,r
 		if (typeof success !== "undefined") {
 			success.get("https://www.googleapis.com/oauth2/v1/userinfo?access_token="+success.access_token,{
 			}).done(function(data){
-				console.log(data);
+				NS.setItem("truckuniqueid",data.id,false);
 			});
 		}else{
 			console.log(error);
@@ -85,7 +85,7 @@ define(["jquery","nomadic_storage","jquery-ui","reporting"],function($,NS,JQUI,r
 		if (typeof success !== "undefined") {
 			success.get("https://api.twitter.com/1.1/account/verify_credentials.json",{
 			}).done(function(data){
-				console.log(data);
+				NS.setItem("truckuniqueid",data.id_str,false);
 			});
 		}else{
 			console.log(error);
@@ -95,7 +95,7 @@ define(["jquery","nomadic_storage","jquery-ui","reporting"],function($,NS,JQUI,r
 		if (typeof success !== "undefined") {
 			success.get("https://graph.facebook.com/me?access_token="+success.access_token,{
 			}).done(function(data){
-				console.log(data);
+				NS.setItem("truckuniqueid",data.id,false)
 			});
 		}else{
 			console.log(error);
